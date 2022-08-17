30821fd2cd548a640f87d48be1f2f57be01c3d02
wickynjy@gmail.com <meow@meow>
2022-08-17 08:15:38 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your code here!
+// const element = document.createElement("div");
+
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+// }
+
+// element.append(ul);
+
+// const main = document.getElementById("main");
+// main.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+
+// function stylingUlElements() {
+//     ul.style.height = "max-height";
+//     ul.style.backgroundColor = "#27647B";
+//     ul.style.border = "2px";
+//     ul.style.borderRadius = "5px";
+
+// }
+
+// stylingUlElements()
+
+
+
+// element.className = 'pet-listing dog';
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Wycliffe is the champion";
